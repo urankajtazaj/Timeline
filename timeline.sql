964c0ccb032bd4f0c1e261000da3e3799ddd5693
urankajtazaj <ukajtazi@gmail.com>
2019-05-06 14:28:09 +0200

    updated database

--- timeline.sql
+++ timeline.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 01, 2019 at 11:04 PM
+-- Generation Time: May 06, 2019 at 02:27 PM
 -- Server version: 10.1.37-MariaDB
 -- PHP Version: 7.3.0
 
@@ -25,14 +25,15 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `likes`
+-- Table structure for table `answer`
 --
 
-CREATE TABLE `likes` (
-  `id` int(15) NOT NULL,
-  `userId` int(10) NOT NULL,
-  `postId` int(10) NOT NULL,
-  `status` int(1) NOT NULL
+CREATE TABLE `answer` (
+  `id` int(10) NOT NULL,
+  `post_id` int(10) NOT NULL,
+  `user_id` int(10) NOT NULL,
+  `comment` varchar(250) NOT NULL,
+  `date` datetime DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -56,7 +57,23 @@ INSERT INTO `post` (`id`, `content`, `userId`, `date`) VALUES
 (1, 'Hello world this is a comment on this social network.', 4, '2019-04-01 22:59:38'),
 (2, 'This is SPARTA!', 4, '2019-04-01 22:59:38'),
 (3, 'What a lovel day.', 4, '2019-04-01 22:59:38'),
-(4, 'A nice day it was today.', 4, '2019-04-01 23:01:41');
+(4, 'A nice day it was today.', 4, '2019-04-01 23:01:41'),
+(5, 'kkjjkj', 4, '2019-04-02 20:48:58'),
+(6, 'hello world this is apost', 4, '2019-04-18 20:21:28'),
+(7, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent gravida tempor dapibus. Nulla quis nisi ac enim placerat accumsan vel vel tortor', 4, '2019-04-18 20:50:20');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `post_like`
+--
+
+CREATE TABLE `post_like` (
+  `id` int(15) NOT NULL,
+  `userId` int(10) NOT NULL,
+  `postId` int(10) NOT NULL,
+  `status` int(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -78,22 +95,28 @@ CREATE TABLE `user` (
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `name`, `image`, `bio`) VALUES
-(4, 'uran', '$2y$10$vAhmcqmNDmrcMVgxoUKoNOwXUv1bMPktgcehy8kr0ufVu3GTd84mq', 'James Doe', '', '');
+(4, 'uran', '$2y$10$vAhmcqmNDmrcMVgxoUKoNOwXUv1bMPktgcehy8kr0ufVu3GTd84mq', 'James Doe', 'profile1.jpg', '');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `likes`
+-- Indexes for table `answer`
 --
-ALTER TABLE `likes`
+ALTER TABLE `answer`
   ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `post`
 --
 ALTER TABLE `post`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `post_like`
+--
+ALTER TABLE `post_like`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -107,16 +130,22 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT for table `likes`
+-- AUTO_INCREMENT for table `answer`
 --
-ALTER TABLE `likes`
-  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `answer`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `post`
 --
 ALTER TABLE `post`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `post_like`
+--
+ALTER TABLE `post_like`
+  MODIFY `id` int(15) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `user`
