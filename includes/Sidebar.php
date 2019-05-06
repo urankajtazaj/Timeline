c6e76a918fdab54c6e79b1f9d026ff769476cbc2
u.kajtazaj@abidat.de <ukajtazi@gmail.com>
2019-05-06 17:22:05 +0200

    changed the design

---
+++ includes/Sidebar.php
@@ -0,0 +1,12 @@
+<div class="card">
+    <div class="card-body">
+        <form action="">
+            <div class="form-group">
+                <input type="text" name="search" class="form-control" placeholder="Search" />
+            </div>
+        </form>
+        <a href="<?= Timeline::goToFunction('login', 'logout') ?>">Logout</a>
+        <br><br>
+        <a href="#" class="btn btn-primary">New post</a>
+    </div>
+</div>
--- index.php
+++ index.php
@@ -1,28 +1,26 @@
 <?php
 include 'includes/header.php';
+$posts = PostController::getPosts();
 ?>
-
-<br>
 
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-6">
+        <div class="col-3 sidebar">
+            <?php include 'includes/Sidebar.php' ?>
+        </div>
+        <div class="col-5 pl-0">
             <div class="card">
                 <div class="card-body p-0">
                     <form method="post" action="<?= Timeline::goToFunction('post', 'createPost') ?>">
                         <div class="form-group">
-                            <textarea required class="form-control full-width new-status" name="content" id="content" rows="2" placeholder="#Say something nice"></textarea>
+                            <textarea required class="form-control full-width new-status" name="content" id="content" rows="3" placeholder="#Say something nice"></textarea>
                         </div>
-                        <div class="btn btn-image ml-3" title="Upload image"><i class="far fa-image"></i></div>
-                        <button type="submit" class="corner btn btn-primary btn-circle ml-auto"><i class="fas fa-paper-plane"></i></button>
+                        <div class="btn btn-image ml-3 mt-2" title="Upload image"><i class="far fa-image"></i></div>
+                        <button type="submit" class="btn btn-primary btn-circle float-right mb-3 mr-3"><i class="fas fa-paper-plane"></i></button>
                     </form>
                 </div>
             </div>
-            <br><br><br><br>
             <?php
-
-
-            $posts = PostController::getPosts();
 
             foreach ($posts as $post) {
                 ?>
@@ -30,11 +28,13 @@ include 'includes/header.php';
                     <div class="card-body">
                     <div class="d-flex align-items-center">
                         <div class="profile-pic small d-inline-block">
+                            <?php if ($post->getUser()->getImage()) { ?>
                             <img src="<?= "uploads/" . $post->getUser()->getImage() ?>" alt="">
+                            <?php } ?>
                         </div>
                         <span class="d-inline ml-3">
                             <b class="mr-1"><?= $post->getUser()->getName() ?></b>
-                            &mdash;
+                            -
                             <small class="ml-1 text-muted">
                                 <?= $post->getFormatedDate() ?>
                             </small>
@@ -48,10 +48,9 @@ include 'includes/header.php';
                     </div>
                     </div>
                     <div class="post-footer text-right text-muted">
-                        <span class="btn float-left" id="comment-count-<?= $post->getId() ?>"><i class="far fa-comment"></i></span>
-                        <span class="btn btn-sm float-left">3</span>
-                        <span onclick="handleLike(1, 1, 1, '<?= Timeline::goToFunction("Like", "handleLike") ?>')" class="btn btn-like"><i class="far fa-heart"></i></span>
-                        <span class="btn btn-sm">122</span>
+                        <span class="=" id="comment-count-<?= $post->getId() ?>"><i class="far fa-comment"></i><span class="btn-sm">3</span></span>
+                        <span onclick="handleLike(1, 1, 1, '<?= Timeline::goToFunction("Like", "handleLike") ?>')" class="btn btn-like ml-4"><i class="far fa-heart"></i><span class="btn-sm">122</span></span>
+
                     </div>
                 </div>
                 <?php
@@ -59,11 +58,11 @@ include 'includes/header.php';
 
             ?>
         </div>
-        <div class="col-md-4">
+        <div class="col-md-3 pl-0">
             <div class="card">
                 <div class="card-body">
                     <div class="profile-pic ml-auto mr-auto">
-                        <img src="<?= "uploads/" . Session::Get('user')->getImage() ?>" alt="">
+                        <img src="<?= "uploads/" . Session::Get('user')->getImage() ?>" alt="<?= Session::Get('user')->getName() ?>">
                     </div>
                     <br>
                     <h3 class="text-center"><?= Session::Get('user')->getName() ?></h3>
