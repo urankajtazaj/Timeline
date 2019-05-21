13155e440f54be6cd27dd2358c9147a1b02e4188
Uran Kajtazaj <ukajtazi@gmail.com>
2019-05-21 15:59:52 +0200

    Update post_likes.js

--- assets/js/post_likes.js
+++ assets/js/post_likes.js
@@ -13,11 +13,6 @@ function handleLike(post_id, btn) {
                 $(btn).removeClass("liked");
                 $(btn).find(".count").text(parseInt($(btn).find(".count").text()) - 1);
             }
-        },
-        error: function (a, b, c) {
-            console.log(a);
-            console.log(b);
-            console.log(c);
         }
     })
 }
